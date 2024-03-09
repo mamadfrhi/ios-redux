f4ca5b83f14d61847de20f010b2cf28c7d03e779
iMamad <mohammad.farrahi@oculid.com>
2024-03-09 15:52:13 +0330

    cleaned tests.

--- trivago_challengeTests/SecondaryButtonTests.swift
+++ trivago_challengeTests/SecondaryButtonTests.swift
@@ -20,42 +20,42 @@ final class SecondaryButtonTests: XCTestCase {
     }
     
     func testSecondarySuccess() {
-        assertSnapshot(of: vc, as: .image, record: true)
+        assertSnapshot(of: vc, as: .image)
     }
     
     func testSecondarySuccessTitleUpdate() {
         vc.brandButton.store?.dispatch(action: .setTitle("TEST"))
-        assertSnapshot(of: vc, as: .image, record: true)
+        assertSnapshot(of: vc, as: .image)
     }
     
     func testSecondarySuccessTrailingIcon() {
         vc.brandButton.store?.dispatch(action: .setIcon(brandButtonIcon: .init(iconPosition: .trailing)))
-        assertSnapshot(of: vc, as: .image, record: true)
+        assertSnapshot(of: vc, as: .image)
     }
     
     func testSecondarySuccessLeadingIcon() {
         vc.brandButton.store?.dispatch(action: .setIcon(brandButtonIcon: .init(iconPosition: .leading)))
-        assertSnapshot(of: vc, as: .image, record: true)
+        assertSnapshot(of: vc, as: .image)
     }
     
     func testSecondarySuccessHighlighted() {
         vc.brandButton.store?.dispatch(action: .setHighlight(isHighlighted: true))
-        assertSnapshot(of: vc, as: .image, record: true)
+        assertSnapshot(of: vc, as: .image)
     }
     
     func testSecondarySuccessTypeUpdate() {
         vc.brandButton.store?.dispatch(action: .setButtonType(buttonType: .actionButton))
-        assertSnapshot(of: vc, as: .image, record: true)
+        assertSnapshot(of: vc, as: .image)
     }
     
     func testSecondarySuccessOrderUpdate() {
         vc.brandButton.store?.dispatch(action: .setButtonOrder(buttonOrder: .secoundary))
-        assertSnapshot(of: vc, as: .image, record: true)
+        assertSnapshot(of: vc, as: .image)
     }
     
     func testSecondarySuccessDisableState() {
         vc.brandButton.store?.dispatch(action: .setDisableStyle(isDisabled: true))
-        assertSnapshot(of: vc, as: .image, record: true)
+        assertSnapshot(of: vc, as: .image)
     }
 }
 
