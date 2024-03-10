8370ef0d73550bf5cf15b17f6c08c295b05aa947
iMamad <mohammad.farrahi@oculid.com>
2024-03-10 18:42:15 +0330

    cleaned sceneDelegate

--- trivago_challenge/Delegates/SceneDelegate.swift
+++ trivago_challenge/Delegates/SceneDelegate.swift
@@ -16,15 +16,17 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
-        let viewModel = ButtonViewModel(initialState: PrimaryState(),
-                                        primaryRenderer:
-                                            PrimaryButtonRenderer(styleCalculator: PrimaryButtonStyleCalculator(),
-                                                                  disableStyleCalculator: ButtonDisableStyleCalculator(),
-                                                                  iconCalculator: PrimaryButtonIconCalculater()),
-                                        secondaryRenderer:
-                                            SecondaryButtonRenderer(styleCalculator: SecondaryButtonStyleCalculator(),
-                                                                    disableStyleCalculator: ButtonDisableStyleCalculator(),
-                                                                    iconCalculator: SecondaryButtonIconCalculater()))
+        let viewModel =
+        ButtonViewModel(
+            initialState: PrimaryState(),
+            primaryRenderer:
+                PrimaryButtonRenderer(styleCalculator: PrimaryButtonStyleCalculator(),
+                                      disableStyleCalculator: ButtonDisableStyleCalculator(),
+                                      iconCalculator: PrimaryButtonIconCalculater()),
+            secondaryRenderer:
+                SecondaryButtonRenderer(styleCalculator: SecondaryButtonStyleCalculator(),
+                                        disableStyleCalculator: ButtonDisableStyleCalculator(),
+                                        iconCalculator: SecondaryButtonIconCalculater()))
         
         
         if useSwiftUI {
