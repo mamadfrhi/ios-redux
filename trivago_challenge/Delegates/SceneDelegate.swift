c6bae6b3adc3a5647920eb00d5dbf1050615664d
iMamad <mohammad.farrahi@oculid.com>
2024-03-10 19:01:55 +0330

    set default start point of the app to UIKit.

--- trivago_challenge/Delegates/SceneDelegate.swift
+++ trivago_challenge/Delegates/SceneDelegate.swift
@@ -11,7 +11,7 @@ import SwiftUI
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     
     var window: UIWindow?
-    let useSwiftUI = true
+    let useSwiftUI = false
     
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
