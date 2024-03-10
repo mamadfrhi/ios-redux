d3949fe5f43d59868e406c5cf9255ff0d073ce8e
iMamad <mohammad.farrahi@oculid.com>
2024-03-10 18:58:23 +0330

    cleaned BrandPrimaryButtonColors class.

--- trivago_challenge/Resources/ButtonColors/BrandPrimaryButtonColors.swift
+++ trivago_challenge/Resources/ButtonColors/BrandPrimaryButtonColors.swift
@@ -10,12 +10,17 @@ import UIKit
 struct BrandPrimaryButtonColors {
     
     //MARK: Success
-    static let primarySuccessButtonBackgroundColor = UIColor(named: "primary-success-btn-back-color")! // #428500;
+    static let primarySuccessButtonBackgroundColor =
+    UIColor(named: "primary-success-btn-back-color")! // #428500;
     
-    static let primarySuccessButtonHighlightColor  = UIColor(named: "primary-success-btn-highlight-color")! // #316300
+    static let primarySuccessButtonHighlightColor =
+    UIColor(named: "primary-success-btn-highlight-color")! // #316300
     
     //MARK: Action
-    static let primaryActionButtonBackgroundColor = UIColor(named: "primary-action-btn-back-color")! // #007FAD
-    static let primaryActionButtonHighlightColor  = UIColor(named: "primary-action-btn-highlight-color")! // #005F81
+    static let primaryActionButtonBackgroundColor =
+    UIColor(named: "primary-action-btn-back-color")! // #007FAD
+    
+    static let primaryActionButtonHighlightColor  =
+    UIColor(named: "primary-action-btn-highlight-color")! // #005F81
 }
 
